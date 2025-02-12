8241d448e080857c839ca5be10b557124a8e3c99
abdmz162 <abdmz.uetian23@gmail.com>
2025-02-12 18:53:48 +0500

    lab_3_task_1 submission

---
+++ lab_3/README.md
@@ -0,0 +1,6 @@
+# LAB_3:
+Testing the given logic gate circuit using questasim
+## GIVEN CIRCUIT:
+![given_circuit diagram](./docs/dsd_circuit_given.png)
+## WAVEFORM OF TESTBENCH:
+![testbench_waveforms](./docs/lab3_task1_wavetest.png)
---
+++ lab_3/rtl/lab3_task1.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module lab3_task1(
+        input logic a,b,c,output logic x,y
+    );
+    logic not_c,a_or_b,a_nand_b,xor_1;
+    assign not_c=~c;
+    assign a_or_b=a|b;
+    assign a_nand_b=~(a&b);
+    assign xor_1=a_nand_b^a_or_b;
+    assign x=not_c^a_or_b;
+    assign y=a_or_b&xor_1;
+endmodule
